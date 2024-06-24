bd927869bf2e0d5ba3eab25d394cf9d13e0f0561
timothyonoja1 <timothyonoja1@gmail.com>
2024-06-24 02:52:05 -0700

    Add color theme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -8,6 +8,52 @@ const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
+    primary: '#415f91',
+    secondary: '#565f71',
+    tertiary: '#705575',
+    error: '#ba1a1a',
+
+    onPrimary: '#ffffff',
+    onSecondary: '#ffffff',
+    onTertiary: '#ffffff',
+    onError: '#ffffff',
+
+    primaryContainer: '#d6e3ff',
+    secondaryContainer: '#dae2f9',
+    tertiaryContainer: '#fad8fd',
+    errorContainer: '#ffdad6',
+
+    onPrimaryContainer: '#001b3e',
+    onSecondaryContainer: '#131c2b',
+    onTertiaryContainer: '#28132e',
+    onErrorContainer: '#410002',
+
+    surfaceDim: '#d9d9e0',
+    surface: '#f9f9ff',
+    surfaceBright: '#f9f9ff',
+    inverseSurface: '#2e3036',
+
+    surfContainerLowest: '#ffffff',
+    surContainerLow: '#f3f3fa',
+    surfContainer: '#ededf4',
+    surfContainerHigh: '#e7e8ee',
+    surfContainerHighest: '#e2e2e9',
+    inverseOnSurface: '#e2e2e9',
+    inversePrimary: '#aac7ff',
+
+    onSurface: '#191c20',
+    onSurfaceVar: '#44474e',
+    outline: '#74777f',
+    outineVariant: '#c4c6d0',
+    scrim: "#000000",
+    shadow: '#000000',
+
+    customColor1: '#1a6b51',
+    onCustomColor1: '#ffffff',
+    customColor1Container: '#a6f2d2',
+    onCustomColor1Container: '#002116',
+
+
     text: '#11181C',
     background: '#fff',
     tint: tintColorLight,
@@ -16,6 +62,51 @@ export const Colors = {
     tabIconSelected: tintColorLight,
   },
   dark: {
+    primary: '#aac7ff',
+    secondary: '#bec6dc',
+    tertiary: '#ddbce0',
+    error: '#ffb4ab',
+
+    onPrimary: '#0a305f',
+    onSecondary: '#283141',
+    onTertiary: '#3f2844',
+    onError: '#690005',
+
+    primaryContainer: '#284777',
+    secondaryContainer: '#3e4759',
+    tertiaryContainer: '#573e5c',
+    errorContainer: '#93000a',
+
+    onPrimaryContainer: '#d6e3ff',
+    onSecondaryContainer: '#dae2f9',
+    onTertiaryContainer: '#fad8fd',
+    onErrorContainer: '#ffdad6',
+
+    surfaceDim: '#111318',
+    surface: '#111318',
+    surfaceBright: '#37393e',
+    inverseSurface: '#e2e2e9',
+
+    surfContainerLowest: '#0c0e13',
+    surContainerLow: '#191c20',
+    surfContainer: '#1d2024',
+    surfContainerHigh: '#282a2f',
+    surfContainerHighest: '#33353a',
+    inverseOnSurface: '#2e3036',
+    inversePrimary: '#415f91',
+
+    onSurface: '#e2e2e9',
+    onSurfaceVar: '#c4c6d0',
+    outline: '#8e9099',
+    outineVariant: '#44474e',
+    scrim: "#000000",
+    shadow: '#000000',
+
+    customColor1: '#8ad6b6',
+    onCustomColor1: '#003828',
+    customColor1Container: '#00513b',
+    onCustomColor1Container: '#a6f2d2',
+
     text: '#ECEDEE',
     background: '#151718',
     tint: tintColorDark,
